bf1439de715e224efd13d60833e8405cf83c706f
Macbook <macbook@Macbooks-MacBook-Air.local>
2024-04-08 21:23:28 +0700

    new

--- src/pages/EditBerita.jsx
+++ src/pages/EditBerita.jsx
@@ -25,7 +25,7 @@ useEffect(() => {
     .then(response => {
         const data = response.data
         const datas = Object.values(data)
-        const item = datas.filter(dataa => dataa.id.includes(id));
+        const item = datas.filter(dataa => dataa.id == id);
         setBerita(item)
         setPar1(item[0].par1)
         setPar2(item[0].par2)
@@ -53,7 +53,6 @@ const Edit = () =>{
         navigate("/tambah-berita"),
         window.location.reload()
     )
-    
 }
 
     return(
