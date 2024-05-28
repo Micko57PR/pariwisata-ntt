62372896b5f8d04a14ee6e16b519f75ae3c4369d
Macbook <macbook@Macbooks-MacBook-Air.local>
2024-05-28 22:54:02 +0700

    update terbaru

--- src/pages/WisataPage.jsx
+++ src/pages/WisataPage.jsx
@@ -72,8 +72,8 @@ export default function WisataPage(){
      {latitude && longitude != null ? <>
     <MapContainer className="mt-5 rounded" center={[latitude, longitude]} zoom={13} scrollWheelZoom={false} style={{height:"300px"}}>
     <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+        url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
     />
     <Marker position={[latitude, longitude]}>
       <Popup>
